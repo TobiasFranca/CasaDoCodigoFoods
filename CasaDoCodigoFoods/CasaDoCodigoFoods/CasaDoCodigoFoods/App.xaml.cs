20291ec9b642358613994f42f4c691a3f1f8f878
Tobias França <tobiasfranca.info@gmail.com>
2017-08-05 17:04:27 -0300

    Icones UWP

--- CasaDoCodigoFoods/CasaDoCodigoFoods/CasaDoCodigoFoods/App.xaml.cs
+++ CasaDoCodigoFoods/CasaDoCodigoFoods/CasaDoCodigoFoods/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasaDoCodigoFoods.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace CasaDoCodigoFoods
         {
             InitializeComponent();
 
-            MainPage = new Pages.MenuPage();
+            MainPage = new NavigationPage(new MenuPage());
         }
 
         protected override void OnStart()
