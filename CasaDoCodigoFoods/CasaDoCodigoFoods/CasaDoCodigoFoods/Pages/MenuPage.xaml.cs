1ae7f2b359c95cb870209ad860f08269ff646a75
Tobias França <tobiasfranca.info@gmail.com>
2017-08-05 16:51:15 -0300

    Adicionado paginas de entregadores e gracons

---
+++ CasaDoCodigoFoods/CasaDoCodigoFoods/CasaDoCodigoFoods/Pages/MenuPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CasaDoCodigoFoods.Pages.Entregadores;
+using CasaDoCodigoFoods.Pages.Garcons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CasaDoCodigoFoods.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GarconsOnClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new GarconsPage());
+        }
+
+        private async void EntregadoresOnClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new EntregadoresPage());
+        }
+    }
+}
